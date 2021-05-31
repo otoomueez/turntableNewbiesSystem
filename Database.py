99a156aa8f7eea333d565df7bea4d2f47541d592
raphaelerbynn <35157794+raphaelerbynn@users.noreply.github.com>
2021-05-31 06:43:43 +0000

    Add files via upload

--- Database.py
+++ Database.py
@@ -5,12 +5,12 @@ try:
         def __init__(self):
             self.query = """CREATE TABLE IF NOT EXISTS Applicants(
                     f_name TEXT NOT NULL,
-                    m_name TEXT ,
+                    m_name TEXT,
                     l_name TEXT NOT NULL,
                     gender TEXT NOT NULL,
                     email TEXT PRIMARY KEY NOT NULL,
                     telephone TEXT,
-                    year_of_graduation INTEGER NOT NULL,
+                    year_of_graduation INTEGER,
                     school TEXT,
                     application_type TEXT NOT NULL,
                     upload_resume BLOB NOT NULL
@@ -25,7 +25,11 @@ try:
 
             self.clear_all_query = """DELETE FROM Applicants"""
 
-            self.get_data_query = """SELECT * FROM Applicants"""
+            self.get_data_query = """SELECT f_name, m_name, l_name, gender, 
+                                            email, telephone, year_of_graduation, 
+                                            school, application_type, upload_resume 
+                                            FROM Applicants"""
+
 
         def create_table(self):
             self.open_connection()
@@ -33,10 +37,12 @@ try:
             self.close_connection()
             return 0
 
-        def set_data_to_admin(self, data):
+        def search_data(self, field, value):
             self.open_connection()
-
+            self.cursor.execute("SELECT * FROM Applicants WHERE %s LIKE '%s'" % (field, value))
+            result = self.cursor.fetchall()
             self.close_connection()
+            return result
 
         def insert_data_to_db(self, data):
             self.open_connection()
